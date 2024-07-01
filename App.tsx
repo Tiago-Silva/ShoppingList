63f5649a38b5856fabd6b6a74e9f9d6c5ee788f2
Tiago <ti.almeida58@gmail.com>
2024-07-01 09:28:12 -0300

    feat: creating theme provider to context

--- App.tsx
+++ App.tsx
@@ -1,32 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {ThemeProvider} from "styled-components";
+import {ThemeProvider, useTheme} from "styled-components";
 import styled from "styled-components/native";
-import theme from "./src/global/theme";
 
 export default function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </Container>
-    </ThemeProvider>
-  );
+    const { theme } = useTheme();
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container>
+                <Text>Open up App.tsx to start working on your app!</Text>
+                <StatusBar style="auto" />
+            </Container>
+        </ThemeProvider>
+    );
 }
 
 const Container = styled.View`
-  flex: 1;
-  background-color: ${props => props.theme.colors.background};
-  align-items: center;
-  justify-content: center;
+    flex: 1;
+    background-color: ${props => props.theme.colors.background};
+    align-items: center;
+    justify-content: center;
 `;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
--- src/global/theme.ts
+++ src/global/theme.ts
@@ -1,6 +1,6 @@
 
 
-export default {
+export const lightTheme = {
     colors: {
         primary: '#5636D3',
         secondary: '#FF872C',
@@ -23,11 +23,31 @@ export default {
         background_header: '#2a2e34',
         background_promotion: '#30353c',
 
-    },
+    }
+}
 
-    // fonts: {
-    //     regular: 'Poppins_400Regular',
-    //     medium: 'Poppins_500Medium',
-    //     bold: 'Poppins_700Bold'
-    // }
+export const darkTheme = {
+    colors: {
+        primary: '#5636D3',
+            secondary: '#FF872C',
+
+            secondary_light: 'rgba(255, 135, 44, 0.3)',
+            success: '#12A454',
+            success_light: 'rgba(18, 164, 84, 0.5)',
+
+            attention: '#E83F5B',
+            attention_light: 'rgba(232, 63, 91, 0.5)',
+
+            shape: '#FFFFFF',
+            title: '#FFE600',
+            text: '#F6C015',
+            text_bar: '#969cB2',
+            text_dark: '#000000',
+
+            background: '#F0F2F5',
+            background_card: '#303035',
+            background_header: '#2a2e34',
+            background_promotion: '#30353c',
+
+    }
 }
