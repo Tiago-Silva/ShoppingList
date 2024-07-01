ebebe32d0511373ec4e304e38c288c7f91b93a1d
Tiago <ti.almeida58@gmail.com>
2024-07-01 09:03:16 -0300

    raw: add global theme.ts

--- App.tsx
+++ App.tsx
@@ -1,14 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {ThemeProvider} from "styled-components";
+import styled from "styled-components/native";
+import theme from "./src/global/theme";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </Container>
+    </ThemeProvider>
   );
 }
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${props => props.theme.colors.background};
+  align-items: center;
+  justify-content: center;
+`;
 
 const styles = StyleSheet.create({
   container: {
