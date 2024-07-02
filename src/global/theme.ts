e5c45132c947a5a2ad3e256a731c62cfd5881898
Tiago <ti.almeida58@gmail.com>
2024-07-02 14:58:34 -0300

    style: adjusting background color

--- src/global/theme.ts
+++ src/global/theme.ts
@@ -29,25 +29,25 @@ export const lightTheme = {
 export const darkTheme = {
     colors: {
         primary: '#5636D3',
-            secondary: '#FF872C',
+        secondary: '#FF872C',
 
-            secondary_light: 'rgba(255, 135, 44, 0.3)',
-            success: '#12A454',
-            success_light: 'rgba(18, 164, 84, 0.5)',
+        secondary_light: 'rgba(255, 135, 44, 0.3)',
+        success: '#12A454',
+        success_light: 'rgba(18, 164, 84, 0.5)',
 
-            attention: '#E83F5B',
-            attention_light: 'rgba(232, 63, 91, 0.5)',
+        attention: '#E83F5B',
+        attention_light: 'rgba(232, 63, 91, 0.5)',
 
-            shape: '#FFFFFF',
-            title: '#FFE600',
-            text: '#F6C015',
-            text_bar: '#969cB2',
-            text_dark: '#000000',
+        shape: '#FFFFFF',
+        title: '#FFE600',
+        text: '#608fe6',
+        text_bar: '#969cB2',
+        text_dark: '#000000',
 
-            background: '#F0F2F5',
-            background_card: '#303035',
-            background_header: '#2a2e34',
-            background_promotion: '#30353c',
+        background: '#1d1f22',
+        background_card: '#303035',
+        background_header: '#1d1f22',
+        gray_01: '#30353c',
 
     }
 }
