5b48d59ff56e37c1dde0fcf7f4f8fcb9e91caedf
Tiago <ti.almeida58@gmail.com>
2024-07-04 14:52:41 -0300

    style: adjusting space

--- src/screens/list/index.tsx
+++ src/screens/list/index.tsx
@@ -1,27 +1,24 @@
-import Button from "../../components/button";
-import {StackNavigationProp} from "@react-navigation/stack";
-import {RootStackParamList} from "../../types/types";
-import {useNavigation} from "@react-navigation/native";
 import * as S from "./styles";
+import React from "react";
+import {Input} from "../../components/input";
+import Button from "../../components/button";
 
-type NavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
 
 export const List = () => {
-    const navigation = useNavigation<NavigationProp>();
-
-    const handleNavigation = () => {
-        navigation.reset({
-            index: 0,
-            routes: [{name: 'Home'}]
-        })
-    }
-
     return (
         <S.Container>
-            <Button
-                title="Home"
-                handleNavigation={handleNavigation}
-            />
+            <S.Header>
+                <S.Title>
+                    Alguma imagem
+                </S.Title>
+                <Input
+                    placeholder={'Nova Lista'}
+                />
+            </S.Header>
+
+            <S.Footer>
+                <Button title={'CRIAR'} handleNavigation={() => {}} />
+            </S.Footer>
         </S.Container>
     );
 }
--- src/screens/list/styles.ts
+++ src/screens/list/styles.ts
@@ -1,6 +1,27 @@
 import styled from "styled-components/native";
+import {RFPercentage} from "react-native-responsive-fontsize";
 
 
 export const Container = styled.View`
     flex: 1;
+    
+    justify-content: space-between;
+
+    padding: ${RFPercentage(3)}px ${RFPercentage(2)}px ${RFPercentage(3)}px ${RFPercentage(2)}px;
+    
+    background-color: ${props => props.theme.colors.background};
+`;
+
+export const Title = styled.Text`
+    color: ${({ theme }: any) => theme.colors.shape};
+    font-size: ${RFPercentage(3)}px;
+    text-align: center;
+`;
+
+export const Header = styled.View`
+    gap: 20px;
+`;
+
+export const Footer = styled.View`
+    width: 100%;
 `;
