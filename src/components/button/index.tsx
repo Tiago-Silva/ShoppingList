a97b7dd337fca6aab8ca2542b8d29c992d7005fd
Tiago <ti.almeida58@gmail.com>
2024-07-04 14:39:46 -0300

    style: adding props width

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,15 +3,17 @@ import * as S from "./styles";
 
 interface Props {
     title: string;
+    width?: number;
     handleNavigation: () => void;
 }
 
 const Button = ({
     title,
+    width = 100,
     handleNavigation
 }: Props) => {
     return (
-        <S.Container onPress={handleNavigation}>
+        <S.Container onPress={handleNavigation} $width={width}>
             <S.Title>{title}</S.Title>
         </S.Container>
     );
--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -1,10 +1,13 @@
 import styled from "styled-components/native";
 import {RFPercentage, RFValue} from "react-native-responsive-fontsize";
 
+interface ButtonProps {
+    $width?: number;
+}
 
-export const Container = styled.TouchableOpacity`
-    width: 50%;
-    height: ${RFPercentage(8)}px;
+export const Container = styled.TouchableOpacity<ButtonProps>`
+    width: ${props => props.$width}%;
+    height: ${RFPercentage(5)}px;
     border: 1px solid ${props => props.theme.colors.gray_01};
     background-color: ${props => props.theme.colors.gray_01};
     border-radius: ${RFValue(30)}px;
@@ -14,6 +17,6 @@ export const Container = styled.TouchableOpacity`
 
 export const Title = styled.Text`
     color: ${props => props.theme.colors.text};
-    font-size: 20px;
+    font-size: ${RFValue(16)}px;
     font-weight: bold;
 `;
