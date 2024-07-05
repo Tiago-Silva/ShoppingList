a643e439a6328a169e7ef29f3357ce3cff733841
Tiago <ti.almeida58@gmail.com>
2024-07-05 14:35:43 -0300

    feat: creating input and list suggestions

--- src/screens/list/index.tsx
+++ src/screens/list/index.tsx
@@ -1,23 +1,61 @@
 import * as S from "./styles";
-import React from "react";
+import React, {useState} from "react";
 import {Input} from "../../components/input";
 import Button from "../../components/button";
+import {ListRenderItemInfo} from "react-native";
 
+export interface Sugestion {
+    id: number;
+    title: string;
+}
+
+const suggestions: Sugestion[] = [
+    { id: 1, title: 'Compras' },
+    { id: 2, title: 'Mantimentos' },
+    { id: 3, title: 'Fim de semana' },
+    { id: 4, title: 'Sexta-feira' },
+    { id: 5, title: 'Viagem' },
+    { id: 6, title: 'Supermercado' },
+    { id: 7, title: 'Casa' },
+    { id: 8, title: 'Farmácia' },
+];
 
 export const List = () => {
+    const [inputValue, setInputValue] = useState('')
+
+    const handleSelectSugestion = (value: string) => {
+        setInputValue(value);
+    };
+
+    const renderItem = ({ item }: ListRenderItemInfo<Sugestion>) => {
+        return (
+            <S.Button onPress={() => handleSelectSugestion(item.title)}>
+                <S.TitleButon>{item.title}</S.TitleButon>
+            </S.Button>
+        )
+    }
+
     return (
         <S.Container>
             <S.Header>
-                <S.Title>
-                    Alguma imagem
-                </S.Title>
+                <S.Icon name={'list-check'} />
                 <Input
                     placeholder={'Nova Lista'}
+                    value={inputValue}
+                    onChangeText={setInputValue}
                 />
+                <S.WrapperList>
+                    <S.TitleButon>Sugestões</S.TitleButon>
+                    <S.WrapperSugestions
+                        data={suggestions}
+                        keyExtractor={(item: Sugestion) => item.id}
+                        renderItem={renderItem}
+                    />
+                </S.WrapperList>
             </S.Header>
 
             <S.Footer>
-                <Button title={'CRIAR'} handleNavigation={() => {}} />
+                <Button title={'CRIAR'} handleOnPress={() => {}} />
             </S.Footer>
         </S.Container>
     );
--- src/screens/list/styles.ts
+++ src/screens/list/styles.ts
@@ -1,6 +1,8 @@
 import styled from "styled-components/native";
-import {RFPercentage} from "react-native-responsive-fontsize";
-
+import {RFPercentage, RFValue} from "react-native-responsive-fontsize";
+import { FontAwesome6 } from '@expo/vector-icons';
+import {FlatList} from "react-native";
+import {Sugestion} from "./index";
 
 export const Container = styled.View`
     flex: 1;
@@ -19,9 +21,56 @@ export const Title = styled.Text`
 `;
 
 export const Header = styled.View`
+    align-items: center;
     gap: 20px;
 `;
 
 export const Footer = styled.View`
     width: 100%;
+`;
+
+export const Icon = styled(FontAwesome6)`
+    font-size: ${RFPercentage(10)}px;
+    color: ${props => props.theme.colors.success};
+`;
+
+export const WrapperList = styled.View`
+    justify-content: flex-start;
+    width: 100%;
+    gap: ${RFPercentage(1.5)}px;
+`;
+
+export const WrapperSugestions = styled(
+    FlatList as new () => FlatList<Sugestion>
+).attrs({
+    horizontal: true,
+    showsVerticalScrollIndicator: false,
+    containerStyle: {
+        paddingRight: RFPercentage(2),
+        paddingLeft: RFPercentage(2)
+    },
+    contentContainerStyle: {
+        gap: RFPercentage(1)
+    }
+})`
+  //margin-top: ${RFPercentage(2)}px;
+  width: 100%;
+`;
+
+export const Button = styled.TouchableOpacity`
+    height: ${RFPercentage(4)}px;
+    border: 1px solid ${props => props.theme.colors.gray_01};
+    background-color: ${props => props.theme.colors.gray_01};
+    border-radius: ${RFValue(30)}px;
+    justify-content: center;
+    align-items: center;
+    flex-grow: 1;
+    
+    padding: 0 ${RFPercentage(2)}px 0 ${RFPercentage(2)}px;
+`;
+
+export const TitleButon = styled.Text`
+    color: ${props => props.theme.colors.text_bar};
+    font-size: ${RFValue(12)}px;
+    font-weight: bold;
 `;
