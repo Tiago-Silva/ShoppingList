ca35cb3d80656d6b0813db4cf967cb1128ea2724
Tiago <ti.almeida58@gmail.com>
2024-07-08 10:01:00 -0300

    raw: creating shoppingService.ts

---
+++ src/components/service/shoppingService.ts
@@ -0,0 +1,26 @@
+import {ShoppingList} from "../interface/interface";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
+export const ShoppingService = {
+
+    save: async (list: ShoppingList) => {
+        try {
+            const shoppingList = await AsyncStorage.getItem('shoppingList');
+            let listsArray = shoppingList ? JSON.parse(shoppingList) : [];
+            listsArray.push(list);
+            await AsyncStorage.setItem('shoppingList', JSON.stringify(listsArray));
+        } catch (error) {
+            console.error('Erro ao salvar a lista no AsyncStorage', error);
+        }
+    },
+
+    getAll: async () => {
+        try {
+            const shoppingList = await AsyncStorage.getItem('shoppingList');
+            return shoppingList ? JSON.parse(shoppingList) : [];
+        } catch (error) {
+            console.error('Erro ao buscar a lista no AsyncStorage', error);
+        }
+    }
+}
