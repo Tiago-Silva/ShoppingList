44857a66ad77ee508e5e5f436d6fde5b0e406c31
Tiago <ti.almeida58@gmail.com>
2024-07-08 07:44:19 -0300

    refactor: animation not usage

--- src/components/animation/RabbitAnimation.tsx
+++ src/components/animation/RabbitAnimation.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import LottieView from 'lottie-react-native';
 
-const RabbitAnimation = () => {
+const animations = {
+    topperRabbit: require('../../../assets/animation/icons/topper-rabbit.json'),
+};
+
+interface Props {
+    animationKey?: keyof typeof animations; // Use the keys of the animations object
+    width?: number;
+    height?: number;
+    top?: number;
+}
+
+const RabbitAnimation = ({
+    animationKey = 'topperRabbit',
+    width = 100,
+    height = 100,
+    top
+}: Props) => {
     return (
         <LottieView
-            source={require('../../../assets/animation/icons/topper-rabbit.json')}
+            source={animations[animationKey]}
             autoPlay
             loop
-            style={{ width: 50, height: 50, top: -20 }}
+            style={
+                {
+                    width: width,
+                    height: height,
+                    top: top
+                }
+            }
+            resizeMode={'cover'}
         />
     );
 };
