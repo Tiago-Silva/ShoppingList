371d0fcc29deab348bfa4e7b586d7521aaaf9991
Tiago <ti.almeida58@gmail.com>
2024-07-09 08:32:50 -0300

    raw: creating type from Header params

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,14 @@
+import {Item} from "../interface/interface";
 
 
 export type RootStackParamList = {
     List: {}; // {} parametros ou undefined
     Home: {};
+    Items: {};
     // Adicione outras rotas aqui...
 }
+
+export type HeaderRouteParams = {
+    name?: string;
+    items?: Item[];
+};
