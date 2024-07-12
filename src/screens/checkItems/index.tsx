40d25c8085d450e97c9c92c1e87d53faedb5d06d
Tiago <ti.almeida58@gmail.com>
2024-07-12 11:12:26 -0300

    feat: adding navigation and retrieving parameters

--- src/screens/checkItems/index.tsx
+++ src/screens/checkItems/index.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import * as S from './styles';
+import * as Progress from 'react-native-progress';
+import {useTheme} from "styled-components";
+import {RouteProp, useNavigation, useRoute} from "@react-navigation/native";
+import {HeaderRouteParams, RootStackParamList} from "../../types/types";
+import Button from "../../components/button";
+import {StackNavigationProp} from "@react-navigation/stack";
+
+type NavigationProp = StackNavigationProp<RootStackParamList>;
+type HeaderRouteProp = RouteProp<{ params: HeaderRouteParams }, 'params'>;
 
 const CheckItems = () => {
+    const theme = useTheme();
+    const navigation = useNavigation<NavigationProp>();
+    const route = useRoute<HeaderRouteProp>();
+    const items = route.params?.items || [];
+
+    const handleNavigationAddItem = () => {
+        navigation.navigate({name: 'AddItems', params: {}});
+    }
+
     return (
         <S.Container>
-            <S.Title>Lista de items</S.Title>
+            <S.Header>
+                <Progress.Bar
+                    progress={0.3}
+                    width={350}
+                    color={theme.colors.text_bar}
+                />
+            </S.Header>
+            <S.Footer>
+                <Button
+                    title={'+ ADICIONAR'}
+                    width={50}
+                    handleOnPress={handleNavigationAddItem}
+                />
+            </S.Footer>
         </S.Container>
     );
 };
--- src/screens/checkItems/styles.ts
+++ src/screens/checkItems/styles.ts
@@ -7,10 +7,22 @@ export const Container = styled.View`
     flex: 1;
     
     justify-content: space-between;
-
-    padding: ${RFPercentage(3)}px ${RFPercentage(2)}px ${RFPercentage(3)}px ${RFPercentage(2)}px;
     
     background-color: ${({ theme }: { theme: themeType }) => theme.colors.background};
+`;
+
+export const Header = styled.View`
+    padding: ${RFPercentage(3)}px ${RFPercentage(2)}px ${RFPercentage(3)}px ${RFPercentage(2)}px;
+    
+    background-color: ${({ theme }: any) => theme.colors.background_card};
+`;
+
+export const Footer = styled.View`
+    width: 100%;
+    flex-direction: row;
+    justify-content: flex-end;
+
+    padding: ${RFPercentage(3)}px ${RFPercentage(2)}px ${RFPercentage(3)}px ${RFPercentage(2)}px;
 `;
 
 export const Title = styled.Text`
