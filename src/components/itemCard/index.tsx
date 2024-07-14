9b744367be96d2cbe729b8fa05e38ec01ca04e44
Tiago <ti.almeida58@gmail.com>
2024-07-14 19:19:08 -0300

    feat: creating ItemCard

---
+++ src/components/itemCard/index.tsx
@@ -0,0 +1,53 @@
+import React, {useEffect, useRef, useState} from 'react';
+import * as S from './styles';
+import IconAnimation from "../animation/IconAnimation";
+import LottieView from "lottie-react-native";
+import {Item} from "../../interface/interface";
+
+interface Props {
+    item: Item
+}
+
+const ItemCard = ({
+    item
+}: Props) => {
+    const animationRef = useRef<LottieView>(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const handleAnimationIcon = () => {
+        if (isPlaying) {
+            setIsPlaying(!isPlaying);
+            animationRef.current?.play();
+            setTimeout(() => {
+                animationRef.current?.pause();
+            }, 2000);
+        } else {
+            setIsPlaying(!isPlaying);
+            animationRef.current?.play();
+            setTimeout(() => {
+                animationRef.current?.pause();
+            }, 1000);
+        }
+    };
+
+    useEffect(() => {
+        handleAnimationIcon();
+    }, []);
+
+    return (
+        <S.Container>
+            <S.WrapperIcon onPress={handleAnimationIcon}>
+                <IconAnimation
+                    animationKey={'checkCircle'}
+                    animationRef={animationRef}
+                    width={50}
+                    height={50}
+                    isLoop={false}
+                />
+            </S.WrapperIcon>
+            <S.Title>{item.name}</S.Title>
+        </S.Container>
+    );
+};
+
+export default ItemCard;
---
+++ src/components/itemCard/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components/native";
+import {RFPercentage} from "react-native-responsive-fontsize";
+
+
+export const Container = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+export const WrapperIcon = styled.TouchableOpacity`
+
+`;
+
+export const Title = styled.Text`
+    color: ${({ theme }: any) => theme.colors.shape};
+    font-size: ${RFPercentage(3)}px;
+    text-align: center;
+`;
