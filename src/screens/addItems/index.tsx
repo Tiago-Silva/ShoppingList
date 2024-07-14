c34fa08944227e4134176c36aefa452b6077adee
Tiago <ti.almeida58@gmail.com>
2024-07-14 19:18:49 -0300

    feat: creating screen addItem

---
+++ src/screens/addItems/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './styles';
+
+const AddItems = () => {
+    return (
+        <S.Container>
+            <S.Title>Adicionar items</S.Title>
+        </S.Container>
+    );
+};
+
+export default AddItems;
---
+++ src/screens/addItems/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components/native";
+import {RFPercentage} from "react-native-responsive-fontsize";
+import {themeType} from "../../global/theme";
+
+
+export const Container = styled.View`
+    height: 100%;
+    padding: ${RFPercentage(3)}px ${RFPercentage(2)}px ${RFPercentage(3)}px ${RFPercentage(2)}px;
+    gap: ${RFPercentage(2)}px;
+    background-color: ${({ theme }: { theme: themeType }) => theme.colors.background};
+
+`;
+
+
+export const Title = styled.Text`
+    color: ${({ theme }: any) => theme.colors.shape};
+    font-size: ${RFPercentage(3)}px;
+    text-align: center;
+`;
