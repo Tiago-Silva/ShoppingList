f2aa4f4c41da2554e94a970d6475724625e5cd64
Tiago <ti.almeida58@gmail.com>
2024-07-16 09:32:16 -0300

    raw: creating function for update shoppingList

--- src/service/shoppingService.ts
+++ src/service/shoppingService.ts
@@ -1,4 +1,4 @@
-import {ShoppingList} from "../interface/interface";
+import {ItemData, ShoppingList} from "../interface/interface";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
@@ -15,10 +15,40 @@ export const ShoppingService = {
         }
     },
 
+    update: async (listName: string, newItem: ItemData) => {
+        try {
+            const shoppingList = await AsyncStorage.getItem('shoppingList');
+            let listsArray: ShoppingList[] = shoppingList ? JSON.parse(shoppingList) : [];
+
+            const listIndex = listsArray.findIndex(list => list.name === listName);
+
+            if (listIndex !== -1) {
+                listsArray[listIndex].items.push(newItem);
+
+                await AsyncStorage.setItem('shoppingList', JSON.stringify(listsArray));
+            } else {
+                console.error('Lista não encontrada');
+            }
+        } catch (error) {
+            console.error('Erro ao atualizar a lista no AsyncStorage', error);
+        }
+    },
+
     getAll: async () => {
         try {
             const shoppingList = await AsyncStorage.getItem('shoppingList');
             return shoppingList ? JSON.parse(shoppingList) : [];
+        } catch (error) {
+            console.error('Erro ao buscar a lista no AsyncStorage', error);
+        }
+    },
+
+    getShoppingList: async (name: string) => {
+        try {
+            const shoppingList = await AsyncStorage.getItem('shoppingList');
+            let listsArray = shoppingList ? JSON.parse(shoppingList) : [];
+
+            return listsArray.find((item: ShoppingList) => item.name === name);
         } catch (error) {
             console.error('Erro ao buscar a lista no AsyncStorage', error);
         }
