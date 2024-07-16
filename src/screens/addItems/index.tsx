b6cfecf1e13c77593b9efe51dcab1f3e6fdc527a
Tiago <ti.almeida58@gmail.com>
2024-07-16 09:33:24 -0300

    feat: adding item to ShoppingList

--- src/screens/addItems/index.tsx
+++ src/screens/addItems/index.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import * as S from './styles';
 import {useAppSelector} from "../../store/modules/hooks";
+import {ShoppingService} from "../../service/shoppingService";
+import {RouteProp, useRoute} from "@react-navigation/native";
+import {HeaderRouteParams} from "../../types/types";
+
+type HeaderRouteProp = RouteProp<{ params: HeaderRouteParams }, 'params'>;
 
 const AddItems = () => {
     const inputValue = useAppSelector((state) => state.cart?.inputValue);
+    const name = useRoute<HeaderRouteProp>().params?.name || '';
+
+    const handleAddItem = async () => {
+        if (!inputValue) {
+            return;
+        }
+
+       await ShoppingService.update(name, {name: inputValue, quantity: 1});
+    }
 
     return (
         <S.Container>
-            <S.Title>{inputValue}</S.Title>
+            <S.LineItem>
+                <S.WrapperIcon onPress={handleAddItem}>
+                    <S.Icon name='plus-circle'/>
+                </S.WrapperIcon>
+                <S.Title>{inputValue}</S.Title>
+            </S.LineItem>
         </S.Container>
     );
 };
