c6370f95eb1eb359ed098cc7b4fd69aa7d947b7a
Tiago <ti.almeida58@gmail.com>
2024-08-01 08:40:18 -0300

    test: creating test for component Button

---
+++ src/__tests__/components/button/Button.spec.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import Button from "../../../components/button";
+import { ThemeProvider } from 'styled-components/native';
+import {darkTheme} from "../../../global/theme";
+
+const renderWithTheme = (component: React.ReactElement) => {
+    return render(<ThemeProvider theme={darkTheme}>{component}</ThemeProvider>);
+};
+
+describe('Button Component', () => {
+    it('renders correctly', () => {
+        const { getByText } = renderWithTheme(<Button title="CRIAR" handleOnPress={() => {}} />);
+        expect(getByText('CRIAR')).toBeTruthy();
+    });
+
+    it('displays the correct title', () => {
+        const { getByText } = renderWithTheme(<Button title="CRIAR" handleOnPress={() => {}} />);
+        expect(getByText('CRIAR')).toBeTruthy();
+    });
+
+    it('calls handleOnPress when pressed', () => {
+        const handleOnPress = jest.fn();
+        const { getByText } = renderWithTheme(<Button title="CRIAR" handleOnPress={handleOnPress} />);
+        fireEvent.press(getByText('CRIAR'));
+        expect(handleOnPress).toHaveBeenCalled();
+    });
+
+    it('applies the correct width', () => {
+        const { getByTestId } = renderWithTheme(<Button title="CRIAR" width={200} handleOnPress={() => {}} />);
+        const button = getByTestId('button-container');
+        expect(button.props.style.width).toBe('200%');
+    });
+});
