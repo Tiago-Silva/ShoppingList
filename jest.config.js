6eca1c925cd18a958687146e1c630998272230a1
Tiago <ti.almeida58@gmail.com>
2024-08-05 15:06:50 -0300

    raw: configuration jest-styled-components and configuration perfomance

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,25 @@
 module.exports = {
     preset: "jest-expo",
+    testPathIgnorePatterns: [
+        "/node_modules",
+        "/android",
+        "/ios",
+        "/build",
+        "/dist"
+    ],
     setupFilesAfterEnv: [
-        "@testing-library/jest-native/extend-expect"
+        "@testing-library/jest-native/extend-expect",
+        "jest-styled-components"
     ],
     transformIgnorePatterns: [
         "node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@react-native-community|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-native-svg|@moti)"
+    ],
+    collectCoverageFrom: [
+        "src/**/*.{js,jsx,ts,tsx}",
+        "!src/**/*.d.ts"
+    ],
+    coveragePathIgnorePatterns: [
+        "/node_modules/",
+        "/src/__tests__/"
     ]
 }
