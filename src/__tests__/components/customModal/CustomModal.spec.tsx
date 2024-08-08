49213df6fc9d02eb420412842e4e8dc92a373cb3
Tiago <ti.almeida58@gmail.com>
2024-08-08 10:40:16 -0300

    test: refactor for using composition pattern

--- src/__tests__/components/customModal/CustomModal.spec.tsx
+++ src/__tests__/components/customModal/CustomModal.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent, act, cleanup } from '@testing-library/react-native';
-import CustomModal from '../../../components/customModal';
+import { CustomModal } from '../../../components/customModal';
 import { ThemeProvider } from 'styled-components/native';
 import { darkTheme } from '../../../global/theme';
 
@@ -19,19 +19,31 @@ describe('CustomModal Component', () => {
         jest.useRealTimers();
     });
 
-    it('renders correctly', () => {
-        const { getByText } = renderWithTheme(<CustomModal isVisible={true} title="Modal Title" onClose={() => {}} />);
-        expect(getByText('Modal Title')).toBeTruthy();
+    it('renders correctly theme', () => {
+        const { getByText } = renderWithTheme(
+            <CustomModal.Root isVisible={true} onClose={() => {}}>
+                <CustomModal.Theme title="Modal theme" />
+            </CustomModal.Root>
+        );
+        expect(getByText('Modal theme')).toBeTruthy();
     });
 
-    it('displays the correct title', () => {
-        const { getByText } = renderWithTheme(<CustomModal isVisible={true} title="Modal Title" onClose={() => {}} />);
-        expect(getByText('Modal Title')).toBeTruthy();
+    it('renders correctly rename', () => {
+        const { getByText } = renderWithTheme(
+            <CustomModal.Root isVisible={true} onClose={() => {}}>
+                <CustomModal.Rename title="Modal rename" />
+            </CustomModal.Root>
+        );
+        expect(getByText('Modal rename')).toBeTruthy();
     });
 
     it('calls handleDelete when delete button is pressed', () => {
         const handleDelete = jest.fn();
-        const { getByText } = renderWithTheme(<CustomModal isVisible={true} title="Modal Title" onClose={() => {}} onDelete={handleDelete} />);
+        const { getByText } = renderWithTheme(
+            <CustomModal.Root isVisible={true} onClose={() => {}}>
+                <CustomModal.Rename title="Modal Title" onDelete={handleDelete} />
+            </CustomModal.Root>
+        );
         act(() => {
             fireEvent.press(getByText('Excluir'));
             expect(handleDelete).toHaveBeenCalled();
@@ -40,7 +52,11 @@ describe('CustomModal Component', () => {
 
     it('calls handleRename when rename button is pressed', () => {
         const handleRename = jest.fn();
-        const { getByText } = renderWithTheme(<CustomModal isVisible={true} title="Modal Title" onClose={() => {}} onRename={handleRename} />);
+        const { getByText } = renderWithTheme(
+            <CustomModal.Root isVisible={true} onClose={() => {}}>
+                <CustomModal.Rename title="Modal Title" onRename={handleRename} />
+            </CustomModal.Root>
+        );
         act(() => {
             fireEvent.press(getByText('Renomear'));
             expect(handleRename).toHaveBeenCalled();
@@ -49,7 +65,11 @@ describe('CustomModal Component', () => {
 
     it('calls handleSelectTheme when a theme is selected', () => {
         const handleSelectTheme = jest.fn();
-        const { getByText } = renderWithTheme(<CustomModal isVisible={true} title="Modal Title" onClose={() => {}} onSelectTheme={handleSelectTheme} isTheme={true} />);
+        const { getByText } = renderWithTheme(
+            <CustomModal.Root isVisible={true} onClose={() => {}}>
+                <CustomModal.Theme title="Modal Title" onSelectTheme={handleSelectTheme} />
+            </CustomModal.Root>
+        );
         act(() => {
             fireEvent.press(getByText('Dark'));
             expect(handleSelectTheme).toHaveBeenCalledWith('dark');
