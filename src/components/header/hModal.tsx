1f3f52f46ef3143f90f92939b14ec8f35e7c4d49
Tiago <ti.almeida58@gmail.com>
2024-08-09 14:07:57 -0300

    fix: using correct import headerService

--- src/components/header/hModal.tsx
+++ src/components/header/hModal.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import {CustomModal} from "../customModal";
 import {HeaderService} from "../../service/headerService";
+import {StorageService} from "../../service/storageService";
+import {ThemeService} from "../../service/themeService";
 
 interface Props {
     isVisible?: boolean;
     onShowModal: () => void;
 }
 
+const storageService = new StorageService();
+const themeService = new ThemeService(storageService);
+const headerService = new HeaderService(themeService);
+
 const HModal = ({ isVisible = false, onShowModal }: Props) => {
-    const handleSelectTheme = HeaderService.handleSelectTheme(onShowModal);
+    const handleSelectTheme = headerService.handleSelectTheme(onShowModal);
 
     return (
         <CustomModal.Root isVisible={isVisible} onClose={onShowModal}>
