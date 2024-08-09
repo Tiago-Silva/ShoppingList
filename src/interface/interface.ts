00931ca3a7bfe01c88a33bb27f3cd5f57be22bdf
Tiago <ti.almeida58@gmail.com>
2024-08-09 16:01:32 -0300

    refactor: changing shoppingService to be a class that implements interface

--- src/interface/interface.ts
+++ src/interface/interface.ts
@@ -19,6 +19,11 @@ export interface Sugestion {
     title: string;
 }
 
+export interface IStorageService {
+    getItem(key: string): Promise<string | null>;
+    setItem(key: string, value: string): Promise<void>;
+}
+
 export interface IThemeService {
     getTheme(): Promise<ThemeType>;
     setTheme(theme: ThemeType): Promise<void>;
@@ -35,7 +40,15 @@ export interface IHeaderService {
     getThemeFromRedux: () => string;
 }
 
-export interface IStorageService {
-    getItem(key: string): Promise<string | null>;
-    setItem(key: string, value: string): Promise<void>;
+export interface IShoppingService {
+    save(list: ShoppingList): Promise<void>;
+    delete(listName: string): Promise<void>;
+    updateShoppingListName(oldName: string, newName: string): Promise<void>;
+    addItem(listName: string, newItem: ItemData): Promise<void>;
+    updateItem(listName: string, itemUpdate: ItemData): Promise<void>;
+    updateItemName(listName: string, oldItem: ItemData, newItem: ItemData): Promise<void>;
+    getAll(): Promise<ShoppingList[] | undefined>;
+    getShoppingList(listName: string): Promise<ShoppingList | undefined>;
+    deleteItemFromList(listName: string, itemName: string): Promise<void>;
+    updateListInReducer(list: ShoppingList | undefined): void;
 }
--- src/service/shoppingService.ts
+++ src/service/shoppingService.ts
@@ -1,41 +1,60 @@
-import {ItemData, ShoppingList} from "../interface/interface";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import {IShoppingService, IStorageService, ItemData, ShoppingList} from "../interface/interface";
+import {useAppDispatch} from "../store/modules/hooks";
+import {updateShoppingList} from "../store/modules/shoppingList/actions";
 
 
-export const ShoppingService = {
+export class ShoppingService implements IShoppingService {
+    private storageService: IStorageService;
+    private dispatch: ReturnType<typeof useAppDispatch>;
 
-    save: async (list: ShoppingList) => {
+    constructor(storageService: IStorageService, dispatch: ReturnType<typeof useAppDispatch>) {
+        this.storageService = storageService;
+        this.dispatch = dispatch;
+    }
+
+    async save (list: ShoppingList): Promise<void> {
         try {
-            const shoppingList = await AsyncStorage.getItem('shoppingList');
+            const shoppingList = await this.storageService.getItem('shoppingList');
             let listsArray = shoppingList ? JSON.parse(shoppingList) : [];
             listsArray.push(list);
-            await AsyncStorage.setItem('shoppingList', JSON.stringify(listsArray));
+            await this.storageService.setItem('shoppingList', JSON.stringify(listsArray));
         } catch (error) {
             console.error('Erro ao salvar a lista no AsyncStorage', error);
         }
-    },
+    }
 
-    updateShoppingListName: async (oldName: string, newName: string) => {
+    async delete (listName: string): Promise<void> {
         try {
-            const shoppingList = await AsyncStorage.getItem('shoppingList');
+            const shoppingList = await this.storageService.getItem('shoppingList');
+            let listsArray = shoppingList ? JSON.parse(shoppingList) : [];
+            listsArray = listsArray.filter((item: ShoppingList) => item.name !== listName);
+            await this.storageService.setItem('shoppingList', JSON.stringify(listsArray));
+        } catch (error) {
+            console.error('Erro ao deletar a lista no AsyncStorage', error);
+        }
+    }
+
+    async updateShoppingListName (oldName: string, newName: string): Promise<void> {
+        try {
+            const shoppingList = await this.storageService.getItem('shoppingList');
             let listsArray: ShoppingList[] = shoppingList ? JSON.parse(shoppingList) : [];
 
             const listIndex = listsArray.findIndex(list => list.name === oldName);
 
             if (listIndex !== -1) {
                 listsArray[listIndex].name = newName;
-                await AsyncStorage.setItem('shoppingList', JSON.stringify(listsArray));
+                await this.storageService.setItem('shoppingList', JSON.stringify(listsArray));
             } else {
                 console.error('Lista não encontrada');
             }
         } catch (error) {
             console.error('Erro ao salvar a lista no AsyncStorage', error);
         }
-    },
+    }
 
-    addItem: async (listName: string, newItem: ItemData) => {
+    async addItem (listName: string, newItem: ItemData): Promise<void> {
         try {
-            const shoppingList = await AsyncStorage.getItem('shoppingList');
+            const shoppingList = await this.storageService.getItem('shoppingList');
             let listsArray: ShoppingList[] = shoppingList ? JSON.parse(shoppingList) : [];
 
             const listIndex = listsArray.findIndex(list => list.name === listName);
@@ -43,18 +62,18 @@ export const ShoppingService = {
             if (listIndex !== -1) {
                 listsArray[listIndex].items.push(newItem);
 
-                await AsyncStorage.setItem('shoppingList', JSON.stringify(listsArray));
+                await this.storageService.setItem('shoppingList', JSON.stringify(listsArray));
             } else {
                 console.error('Lista não encontrada');
             }
         } catch (error) {
             console.error('Erro ao atualizar a lista no AsyncStorage', error);
         }
-    },
+    }
 
-    updateItem: async (listName: string, itemUpdate: ItemData) => {
+    async updateItem (listName: string, itemUpdate: ItemData): Promise<void> {
         try {
-            const shoppingList = await AsyncStorage.getItem('shoppingList');
+            const shoppingList = await this.storageService.getItem('shoppingList');
             let listsArray: ShoppingList[] = shoppingList ? JSON.parse(shoppingList) : [];
 
             const listIndex = listsArray.findIndex(list => list.name === listName);
@@ -64,18 +83,18 @@ export const ShoppingService = {
                     ...listsArray[listIndex],
                     items: listsArray[listIndex].items.map(item => item.name === itemUpdate.name ? itemUpdate : item)
                 };
-                await AsyncStorage.setItem('shoppingList', JSON.stringify(listsArray));
+                await this.storageService.setItem('shoppingList', JSON.stringify(listsArray));
             } else {
                 console.error('Lista não encontrada');
             }
         } catch (error) {
             console.error('Erro ao atualizar a lista no AsyncStorage', error);
         }
-    },
+    }
 
-    updateItemName: async (listName: string, oldeItem: ItemData, newItem: ItemData) => {
+   async updateItemName (listName: string, oldItem: ItemData, newItem: ItemData): Promise<void> {
         try {
-            const shoppingList = await AsyncStorage.getItem('shoppingList');
+            const shoppingList = await this.storageService.getItem('shoppingList');
             let listsArray: ShoppingList[] = shoppingList ? JSON.parse(shoppingList) : [];
 
             const listIndex = listsArray.findIndex(list => list.name === listName);
@@ -83,62 +102,58 @@ export const ShoppingService = {
             if (listIndex !== -1) {
                 listsArray[listIndex] = {
                     ...listsArray[listIndex],
-                    items: listsArray[listIndex].items.map(item => item.name === oldeItem.name ? newItem : item)
+                    items: listsArray[listIndex].items.map(item => item.name === oldItem.name ? newItem : item)
                 };
-                await AsyncStorage.setItem('shoppingList', JSON.stringify(listsArray));
+                await this.storageService.setItem('shoppingList', JSON.stringify(listsArray));
             } else {
                 console.error('Lista não encontrada');
             }
         } catch (error) {
             console.error('Erro ao atualizar a lista no AsyncStorage', error);
         }
-    },
+    }
 
-    getAll: async () => {
+    async getAll (): Promise<ShoppingList[] | undefined> {
         try {
-            const shoppingList = await AsyncStorage.getItem('shoppingList');
+            const shoppingList = await this.storageService.getItem('shoppingList');
             return shoppingList ? JSON.parse(shoppingList) : [];
         } catch (error) {
             console.error('Erro ao buscar a lista no AsyncStorage', error);
         }
-    },
+    }
 
-    getShoppingList: async (name: string) => {
+    async getShoppingList (listName: string): Promise<ShoppingList | undefined> {
         try {
-            const shoppingList = await AsyncStorage.getItem('shoppingList');
+            const shoppingList = await this.storageService.getItem('shoppingList');
             let listsArray = shoppingList ? JSON.parse(shoppingList) : [];
 
-            return listsArray.find((item: ShoppingList) => item.name === name);
+            return listsArray.find((item: ShoppingList) => item.name === listName);
         } catch (error) {
             console.error('Erro ao buscar a lista no AsyncStorage', error);
         }
-    },
+    }
 
-    delete: async (list: ShoppingList) => {
+    async deleteItemFromList (listName: string, itemName: string): Promise<void> {
         try {
-            const shoppingList = await AsyncStorage.getItem('shoppingList');
-            let listsArray = shoppingList ? JSON.parse(shoppingList) : [];
-            listsArray = listsArray.filter((item: ShoppingList) => item.name !== list.name);
-            await AsyncStorage.setItem('shoppingList', JSON.stringify(listsArray));
-        } catch (error) {
-            console.error('Erro ao deletar a lista no AsyncStorage', error);
-        }
-    },
-    deleteItemFromList: async (listName: string, item: ItemData) => {
-        try {
-            const shoppingList = await AsyncStorage.getItem('shoppingList');
+            const shoppingList = await this.storageService.getItem('shoppingList');
             let listsArray: ShoppingList[] = shoppingList ? JSON.parse(shoppingList) : [];
 
-            const listIndex = listsArray.findIndex(l => l.name === listName);
+            const listIndex = listsArray.findIndex((list: ShoppingList) => list.name === listName);
 
             if (listIndex !== -1) {
-                listsArray[listIndex].items = listsArray[listIndex].items.filter(i => i.name !== item.name);
-                await AsyncStorage.setItem('shoppingList', JSON.stringify(listsArray));
+                listsArray[listIndex].items = listsArray[listIndex].items.filter((item: ItemData) => item.name !== itemName);
+                await this.storageService.setItem('shoppingList', JSON.stringify(listsArray));
             } else {
                 console.error('Lista não encontrada');
             }
         } catch (error) {
             console.error('Erro ao deletar o item da lista no AsyncStorage', error);
+        }
+    }
+
+    updateListInReducer(list: ShoppingList | undefined): void {
+        if (list) {
+            this.dispatch(updateShoppingList(list));
         }
     }
 }
