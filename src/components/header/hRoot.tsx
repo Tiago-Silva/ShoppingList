08e7e70e1cecdb8a4ef7403cfa398ef96bb93c28
Tiago <ti.almeida58@gmail.com>
2024-08-21 20:59:57 -0300

    refactor: deleting useState and moving to service

--- src/components/header/hRoot.tsx
+++ src/components/header/hRoot.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import * as S from "./styles";
+import {StorageService} from "../../service/storageService";
+import {ShoppingService} from "../../service/shoppingService";
+import {useAppDispatch} from "../../store/modules/hooks";
 
 interface Props {
     background?: string;
@@ -11,18 +14,21 @@ interface ChildProps {
     isVisible?: boolean;
 }
 
+const storageService = new StorageService();
+
 const HRoot = ({ background = 'background_card', children }: Props) => {
-    const [isVisible, setIsVisible] = useState(false);
+    const shoppingService = new ShoppingService(storageService, useAppDispatch());
+    const isModalVisible = shoppingService.getIsModalVisible();
 
     const handleShowModal = () => {
-        setIsVisible(!isVisible);
+        shoppingService.setIsModalVisibleInReducer(!isModalVisible, false);
     };
 
     return (
         <S.Container $background={background}>
             {React.Children.map(children, child => {
                 if (React.isValidElement<ChildProps>(child)) {
-                    return React.cloneElement(child, { onShowModal: handleShowModal, isVisible });
+                    return React.cloneElement(child, { onShowModal: handleShowModal, isVisible: isModalVisible });
                 }
                 return child;
             })}
